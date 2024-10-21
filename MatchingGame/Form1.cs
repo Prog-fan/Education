e6b0c51cbda201cba52ce39e81ccce1504261b22
Prog-fan <programootka@gmail.com>
2024-10-21 17:17:23 +0500

    release

---
+++ MatchingGame/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+
+namespace MatchingGame
+{
+    public partial class Form1 : Form
+    {
+        string rightly = "D:\\work\\sounds\\pravilno.wav";
+        string wrongly = "D:\\work\\sounds\\no.wav";
+        bool enable = false;
+        int timeleft;
+        Label firstClicked = null;
+        Label secondClicked = null;
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "K", "K",
+            "b", "b", "v", "v", "w", "w", "z", "z",
+            "j", "j", "k", "k", "o", "o", "@", "@",
+            "L", "L", "h", "h", "B", "B", "~", "~",
+            "-", "-", "%", "%"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumer = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumer];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumer);
+                }
+            }
+        }
+
+        private void Label_Click(object sender, EventArgs e)
+        {
+            if (enable == true)
+            {
+                Label clickLabel = sender as Label;
+                if (clickLabel != null)
+                {
+                    if (clickLabel.ForeColor == Color.Black)
+                        return;
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickLabel;
+                        firstClicked.ForeColor = Color.Black;
+                        return;
+                    }
+
+                    if (secondClicked == null)
+                    {
+                        secondClicked = clickLabel;
+                        secondClicked.ForeColor = Color.Black;
+                    }
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        SoundPlayer player = new SoundPlayer(rightly);
+                        player.Play();
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                    else
+                    {
+                        SoundPlayer player = new SoundPlayer(wrongly);
+                        player.Play(); 
+                        timer1.Start();
+                    }
+              
+                }
+            }
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            timeleft = 300;
+            timer2.Start();
+            StartButton.Enabled = false;
+            enable = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+            firstClicked = null;
+            secondClicked = null;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (timeleft > 0)
+            {
+                timeleft--;
+                label37.Text = timeleft + " Seconds";
+                checkForLoss();
+            }
+        }
+
+        private void checkForLoss()
+        {
+            if(timeleft == 0)
+            {
+                timer2.Stop();
+                tableLayoutPanel1.Enabled = false;
+                MessageBox.Show("Time is up!!!", "Ooops"); 
+                Close();
+            }
+        }
+
+        private void CheckForWinner()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+            timer2.Stop();
+            MessageBox.Show("You match all icons!", "Congratulations");
+            Close();
+        }
+    }
+}
---
+++ MatchingGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("MatchingGame")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MatchingGame")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ea9c0ad5-f8da-42bd-87f3-1b38c8bf77c2")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
